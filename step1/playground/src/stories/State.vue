e84672aa8355f2fc11ad002386ddf9375e7eb112
Dueno <anoinchick@gmail.com>
2018-07-27 01:16:28 +0900

    Add "1.3 ライフサイクルフック"

---
+++ step1/playground/src/stories/State.vue
@@ -0,0 +1,16 @@
+<template>
+  <span>{{text}}</span>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: '初期状態'
+    }
+  },
+  created() {
+    this.text = 'created'
+  }
+};
+</script>
--- step1/playground/src/stories/index.js
+++ step1/playground/src/stories/index.js
@@ -1,9 +1,16 @@
 import Vue from "vue";
 import { storiesOf } from "@storybook/vue";
 import MyButton from "./Button.vue";
+import MyState from "./State.vue";
 
 storiesOf("Button", module)
   .add("first button", () => ({
     components: { MyButton },
     template: '<my-button>Chick</my-button>'
+  }));
+
+storiesOf("1.3", module)
+  .add("lifecycle", () => ({
+    components: { MyState },
+    template: '<my-state></my-state>'
   }));
